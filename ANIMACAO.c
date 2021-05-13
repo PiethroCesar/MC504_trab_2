62ab3cac8fc6a8cebb40fb15ef2324b1179303e4
PiethroCesar <piethrocesar@hotmail.com>
2021-05-13 17:40:17 -0300

    separaçao_em_funcoes

--- ANIMACAO.c
+++ ANIMACAO.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    printf("Cena 0 - Ninguem no livro \n\n\n\n");
+
+
+
+
+
+void dois_anjos(){
     printf("        -=-                 -=-\n");
     printf("     (\\  _  /)           (\\  _  /)\n");
     printf("     ( \\( )/ )           ( \\( )/ )\n");
@@ -11,36 +15,48 @@ int main(){
     printf("      /     \\             /     \\\n");
     printf("      '-._.-'             '-._.-'\n");
 
+}
 
+void porta_aberta(){
     printf("_______               ______________________________\n");
     printf("      /\n");
     printf("     /\n");
     printf("    /\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
+}
+
+void pula_linhas(int n){
+    for(int i=0; i<n; i++){
+        printf("\n");
+    }
+}
+
+void livro(){
     printf("              ______ ______\n");
     printf("            _/      Y      \\_\n");
     printf("           // ~~ ~~ | ~~ ~  \\\\\n");
     printf("          // ~ ~ ~~ | ~~~ ~~ \\\\\n");
     printf("         //________.|.________\\\\\n");
     printf("        '----------'-'----------'\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("_______               ______________________________\n");
-    printf("      /\n");
-    printf("     /\n");
-    printf("    /\n");
-    printf("\n");
-    printf("\n");
-    printf("\n");
-    printf("  _____          _____          _____             _____             _____\n");
-    printf("  //_+\\         %% '-' %%        J '-' L           | '-' |           | *-* |\n");
+}
+
+void pessoas(){
+    printf("  _____          _____          _____          ________             _____\n");
+    printf("  //_+\\         %% '-' %%        J O-O L           | `-` |           | *-* |\n");
     printf("  /[\\]\\          /[U]\\         --[:]--            /[Y]\\          / [__I__] \\\n");
     printf(" _/  |_          _I |_          _\\ /_             _| |_           _|     \\_\n");
+}
+
+int main(){
+    printf("Cena 0 - Ninguem no livro \n\n\n\n");
+
+    dois_anjos();
+    porta_aberta();
+    pula_linhas(3);
+    livro();
+    pula_linhas(5);
+    porta_aberta();
+    pula_linhas(3);
+    pessoas();
 
 
     return 0;
