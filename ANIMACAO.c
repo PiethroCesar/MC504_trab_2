2a2491b3b100a490fff6bda080ad3d0c1e341b44
PiethroCesar <piethrocesar@hotmail.com>
2021-05-13 17:12:30 -0300

    primeiro_frame

---
+++ ANIMACAO.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    printf("Cena 0 - Ninguem no livro \n\n\n\n");
+    printf("        -=-                 -=-\n");
+    printf("     (\\  _  /)           (\\  _  /)\n");
+    printf("     ( \\( )/ )           ( \\( )/ )\n");
+    printf("     (       )           (       )\n");
+    printf("      '>   <'             '>   <'\n");
+    printf("      /     \\             /     \\\n");
+    printf("      '-._.-'             '-._.-'\n");
+
+
+    printf("_______               ______________________________\n");
+    printf("      /\n");
+    printf("     /\n");
+    printf("    /\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("              ______ ______\n");
+    printf("            _/      Y      \\_\n");
+    printf("           // ~~ ~~ | ~~ ~  \\\\\n");
+    printf("          // ~ ~ ~~ | ~~~ ~~ \\\\\n");
+    printf("         //________.|.________\\\\\n");
+    printf("        '----------'-'----------'\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf("_______               ______________________________\n");
+    printf("      /\n");
+    printf("     /\n");
+    printf("    /\n");
+    printf("\n");
+    printf("\n");
+    printf("\n");
+    printf(" _____           _____          _____             _____            _____\n");
+    printf("/  / _ + \\        %% ' - ' %%       J  ' - '  L           |  ' - '  |           |  * - *  |\n");
+    printf(" / [  \\  ] \\          / [ U ] \\         --[  :  ]--            / [ Y ] \\           / [  I  ] \\\n");
+    printf(" _/     |_          _I     |_          _\\    /_            _|     |_           _|     \\_\n");
+
+
+    return 0;
+}
